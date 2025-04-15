ac9d7a7eff6d4b5d709aa51f79fd6efabcadef0d
Rohit Nagraj <rohitnagraj.99@gmail.com>
2025-04-15 10:29:52 -0700

    Added offline inference

---
+++ inference.py
@@ -0,0 +1,20 @@
+from MobileVLM.scripts.inference import inference_once
+
+model_path = "mtgv/MobileVLM_V2-3B" # MobileVLM V2
+image_file = "MobileVLM/assets/samples/demo.jpg"
+prompt_str = "Who is the author of this book?\nAnswer the question using a single word or phrase."
+
+args = type('Args', (), {
+    "model_path": model_path,
+    "image_file": image_file,
+    "prompt": prompt_str,
+    "conv_mode": "v1",
+    "temperature": 0, 
+    "top_p": None,
+    "num_beams": 1,
+    "max_new_tokens": 512,
+    "load_8bit": False,
+    "load_4bit": False,
+})()
+
+inference_once(args)
